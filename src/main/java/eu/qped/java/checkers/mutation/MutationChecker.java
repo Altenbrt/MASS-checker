fb60842b456c35f69e25f068bf078a3441217ad6
Tobias Altenbrand <altenbrt@students.uni-marburg.de>
2023-08-31 16:26:59 +0200

    Test38

--- src/main/java/eu/qped/java/checkers/mutation/MutationChecker.java
+++ src/main/java/eu/qped/java/checkers/mutation/MutationChecker.java
@@ -68,7 +68,7 @@ public class MutationChecker {
 		List<String> testClassNames = testClasses.stream().map(MutationFacade::className).collect(Collectors.toList());
 
 
-		Versions[] v;
+		//Versions[] v;
 
 		messages.add("testClassNames: ");
 		messages.add("Classes: ");
@@ -86,14 +86,14 @@ public class MutationChecker {
 				if (Arrays.stream(loadedClass.getInterfaces()).anyMatch(x -> x.getName().contains("Versions"))) {
 					messages.add("It works !!!!!!!!!!!!!!!!!!!!!!!!!!");
 					//Getting the Enum
-					Class<Versions> cV = loadedClass.asSubclass(Versions.class);
+					//Class<Versions> cV = loadedClass.asSubclass(Versions.class);
 
-					v = cV.getEnumConstants();
+					Object[] v = loadedClass.getEnumConstants();
 
 					messages.add("Length: " + v.length);
 
 					//messages.add(v.toString());
-					//Arrays.stream(v).forEach(x->messages.add(x.getFeedback()));
+					/Arrays.stream(v).forEach(x->messages.add(x.toString()));
 				}
 				if (loadedClass.isInstance(BasicTemplate.class))
 					messages.add("It works !!!!!!!!!!!!!!!!!!!!!!!!!!");
